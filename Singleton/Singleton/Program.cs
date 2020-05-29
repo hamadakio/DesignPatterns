acc1d190e75c3b893a0acfd923432876ef0028d4
Akio Hamada <kiyo.akio@gmail.com>
2020-05-29 18:08:53 -0300

    Singleton

---
+++ Singleton/Singleton/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton player_1 = Singleton.GetInstancia;
+            player_1.Mensagem("Jogador1 : A Bola esta comigo");
+
+            Singleton player_2 = Singleton.GetInstancia;
+            player_2.Mensagem("Jogador2 : recebeu a bola");
+
+            Singleton player_3 = Singleton.GetInstancia;
+            player_3.Mensagem("Jogador3 : recebeu o lançamento");
+        }
+    }
+}
---
+++ Singleton/Singleton/Singleton.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    //sealed nao permite a classe ser herdada
+    public sealed class Singleton
+    {
+        private static Singleton Instancia = null;
+
+        public static Singleton GetInstancia
+        { 
+            //garante que exista apenas uma instancia do objeto
+            get
+            {
+                if (Instancia == null)
+                {
+                    Instancia = new Singleton();
+                    Console.WriteLine("Bola em Jogo");
+                }
+
+                return Instancia;
+            }
+
+        }
+
+        public void Mensagem(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+        
+    }
+}
